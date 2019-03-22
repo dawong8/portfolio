81efeea2fd24b56bf01053665cf994ad9fffe897
Danny Wong <wong.danny29@gmail.com>
2019-03-22 01:10:05 -0700

    no one will see this

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('hi')
+
+const $profile = $(".profile-pic");
+const $startScreen = $("#invi");
+
+const $item = $(".project-item");
+const $project = $(".project-item img");
+
+const projectDesc = {
+	flutter: "",
+
+};
+
+
+function makeThirsty() {
+	if ($profile.attr('src') === './public/photo.png') {
+		$profile.attr('src', './public/clip.png').css({'margin-top': '120px'});
+	} else {
+		$profile.attr('src', './public/photo.png').css({'margin-top': '100px'});
+	}
+
+    
+}
+
+
+
+$profile.hover( makeThirsty );
+
