42d1f31f0c73be44e6de315aa226a5ab44fa8b72
Danny Wong <wong.danny29@gmail.com>
2019-04-09 14:49:11 -0700

    changed about me section

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ const projectDesc = {
 };
 
 
-function makeThirsty() {
-	if ($profile.attr('src') === './public/photo.png') {
-		$profile.attr('src', './public/clip.png').css({'margin-top': '120px'});
-	} else {
-		$profile.attr('src', './public/photo.png').css({'margin-top': '100px'});
-	}
+// function makeThirsty() {
+// 	if ($profile.attr('src') === './public/photo.png') {
+// 		$profile.attr('src', './public/clip.png').css({'margin-top': '120px'});
+// 	} else {
+// 		$profile.attr('src', './public/photo.png').css({'margin-top': '100px'});
+// 	}
 
     
-}
+// }
 
 
 
-$profile.hover( makeThirsty );
+// $profile.hover( makeThirsty );
 
